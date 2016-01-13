e7310ab42a15ae804b33d66ef9d5ee395f9c35a1
chrismamo1 <christophermcalpine@gmail.com>
2016-01-13 00:29:36 -0600

    added support for 'small' mode on decompression.

--- zipper.ml
+++ zipper.ml
@@ -3,11 +3,13 @@ let () =
   let oname = ref "" in
   let compress = ref true in
   let decompress = ref false in
+  let small = ref false in
   let speclist = [
     "-file", Arg.Set_string fname, "name of input file";
     "-output", Arg.Set_string oname, "name of output file";
     "--compress", Arg.Set compress, "switch to compress file";
     "--decompress", Arg.Set decompress, "switch to decompress file";
+    "--small", Arg.Set small, "switch to enable 'small' mode, which is slower but more memory efficient";
   ] in
   let () =
     Arg.parse
@@ -18,6 +20,7 @@ let () =
   let oname = if !oname = "" then fname ^ ".bz2" else !oname in
   let decompress = !decompress in
   let compress = if decompress then false else true in
+  let small = !small in
   match compress, decompress with
   | true, false ->
       let ic = open_in fname in
@@ -36,7 +39,7 @@ let () =
   | false, true ->
       let ic = open_in_bin fname in
       let oc = open_out oname in
-      let ic' = Bz2.open_in ic in
+      let ic' = Bz2.open_in ~small ic in
       let buflen = 4096 in
       let buffer = Bytes.create buflen in
       let nread = ref buflen in
